4ff38b4b7b023b1e02d1219df188616343cbd8aa
codeverse-amol <amolbs2109@gmail.com>
2026-06-16 21:36:32 +0530

    Update allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") 
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.getenv("ALLOWED_HOSTS", "").split(",")
+    if host.strip()
+]
 
 
 # Application definition
